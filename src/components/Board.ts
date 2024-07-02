bc9ed600a29d74f19dba04640502cccc064123d7
Alessandro Nesi <alexandronesi@gmail.com>
2024-07-02 07:29:21 +0200

    removed vue-draggable and implemented drag and drop

--- src/components/Board.ts
+++ src/components/Board.ts
@@ -1,29 +1,33 @@
 export class Board {
+  id: number;
   name: string;
   stages: Stage[];
 
-  constructor(name: string, stages: Stage[]) {
+  constructor(name: string, stages: Stage[], id: number) {
+    this.id = id;
     this.name = name;
     this.stages = stages;
   }
 }
 
 export class Stage {
+  id: number;
   name: string;
-  tasks: Task[];
 
-  constructor(name: string, tasks: Task[]) {
+  constructor(name: string, id: number) {
     this.name = name;
-    this.tasks = tasks;
+    this.id = id;
   }
 }
 
 export class Task {
   name: string;
   id: number;
+  stageId: number;
 
-  constructor(name: string, id: number) {
+  constructor(name: string, id: number, stageId: number) {
     this.name = name;
     this.id = id;
+    this.stageId = stageId;
   }
 }
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="pa-5 fill-height">
     <v-row class="fill-height">
-      <v-col cols="3" v-for="stage in board.stages">
+      <v-col cols="3" v-for="stage in board.stages" >
         <v-card
-          class="py-1 fill-height"
+          class="py-1 fill-height drop-zone"
           color="surface-variant"
           rounded="md"
           target="_blank"
-          v-bind:title="stage.name"
-          variant="tonal">
-          <draggable :list="stage.tasks" itemKey="name" group="board" :animation="500">
-            <template #item="{element, index}">
-              <v-card
-                class="pa-1 ma-3"
-                color="surface-variant"
-                rounded="lg"
-                target="_blank"
-                subtitle="Task"
-                :text="element.name"
-                variant="tonal"></v-card>
-            </template>
-          </draggable>
+          :title="stage.name"
+          variant="tonal"
+          @drop="onDrop($event, stage)"
+          @dragover.prevent
+          @dragenter.prevent>
+          <div v-for="task in tasksForStage(stage.id)">
+            <v-card
+              :id="task.id"
+              class="pa-1 ma-3"
+              color="surface-variant"
+              rounded="lg"
+              target="_blank"
+              subtitle="Task"
+              :text="task.name"
+              variant="tonal"
+              draggable="true"
+              @dragstart="startDrag($event, task)"></v-card>
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -30,27 +34,50 @@
 <script setup lang="ts">
 import {ref} from 'vue';
 import {Stage, Board, Task} from './Board.ts';
-import draggable from 'vuedraggable';
-
-const inprogress = new Stage('In progress',[
-  new Task('Bat wing soup', 0),
-  new Task('Spicy Octopus',1),
-  new Task('Anything from Taco Bell',2)
-]);
-
-const review = new Stage('Review', [
-  new Task('Pippo', 0)
+const tasks = ref([
+  new Task('Bat wing soup', 0, 0),
+  new Task('Spicy Octopus',1, 0),
+  new Task('Anything from Taco Bell',2, 0),
+  new Task('Pippo', 3, 1),
+  new Task('Pluto', 4, 2),
+  new Task('Hamburger',5, 3),
+  new Task('Pizza',6, 3),
+  new Task('Spaghetti',7, 3),
+  new Task('Tacos',8, 3),
+  new Task('Teriyaki Chicken', 9, 3)
 ])
 
-const done = new Stage('Done',[
-  new Task('Pluto', 0),
-])
-let todo = new Stage( 'To do', [
-  new Task('Hamburger',0),
-  new Task('Pizza',1),
-  new Task('Spaghetti',2),
-  new Task('Tacos',3),
-  new Task('Teriyaki Chicken', 4)
-]);
-const board = ref(new Board('My Board', [todo, inprogress, review, done]))
+const inprogress = new Stage('In progress', 0);
+const review = new Stage('Review', 1)
+const done = new Stage('Done', 2)
+const todo = new Stage( 'To do', 3);
+const board = ref(new Board('My Board', [todo, inprogress, review, done], 0))
+
+const startDrag = (evt, task: Task) => {
+  evt.dataTransfer.dropEffect = 'move'
+  evt.dataTransfer.effectAllowed = 'move'
+  evt.dataTransfer.setData('taskId', String(task.id))
+  console.log(`${task.id} ${task.stageId}`)
+}
+const onDrop = (evt, stage: Stage) => {
+  const taskId = Number(evt.dataTransfer.getData('taskId'))
+  const task = tasks.value.find(task => task.id === taskId)
+  task.stageId = stage.id
+  console.log(`${task.id} ${task.stageId}`)
+}
+
+const tasksForStage = (stageId: number) => {
+  return tasks.value.filter(task => task.stageId === stageId)
+}
 </script>
+<style scoped>
+.drop-zone {
+
+}
+
+.drag-el {
+  background-color: #fff;
+  margin-bottom: 10px;
+  padding: 5px;
+}
+</style>
